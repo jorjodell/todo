f6fe6e6f90696cce2fa1df9751a5b1d384d7b505
Adylbek Dzhoroev <adikjoro@gmail.com>
2022-05-18 18:11:08 +0600

    Implement another toggle btn variant

--- src/components/ToggleBtn/ToggleBtn.jsx
+++ src/components/ToggleBtn/ToggleBtn.jsx
@@ -3,10 +3,18 @@ import clsx from "clsx";
 
 const ToggleBtn = () => {
   const [isDaily, setIsDaily] = useState(false);
+
   return (
     <div className="px-11">
-      <div className="grid grid-cols-2 h-[50px] rounded-full bg-white">
-        <button
+      <label className="relative grid grid-cols-2 h-[50px] rounded-full bg-white items-center justify-items-center">
+        <div className={clsx("z-10", !isDaily && "text-white")}>Mothly</div>
+        <div className={clsx("z-10", isDaily && "text-white")}>Daily</div>
+        <div className={clsx(
+          "absolute w-1/2 bg-primary rounded-full h-full left-0 transition-transform",
+          isDaily && "translate-x-full"
+        )} />
+        <input type="checkbox" className="sr-only" onChange={(e) => setIsDaily(e.target.checked)} />
+        {/* <button
           className={clsx(
             "h-full",
             !isDaily && "bg-primary text-white rounded-full"
@@ -22,8 +30,8 @@ const ToggleBtn = () => {
           onClick={() => setIsDaily(true)}
         >
           Daily
-        </button>
-      </div> 
+        </button> */}
+      </label> 
     </div>
   )
 }
